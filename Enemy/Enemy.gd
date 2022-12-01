e344369561afd8cb1bb2943081c5c9fc57d070bf
noagird <112106355+noagird@users.noreply.github.com>
2022-12-01 17:41:10 -0500

    Moving Enemy

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,20 +1,30 @@
 extends KinematicBody
 
-var Player = null
+onready var Player = get_node_or_null("/root/Game/Player")
+
+var velocity = Vector2()
+var speed = 100
+var detectedPlayer = false
+var playerPosition = Vector3()
+var enemyPosition = Vector3()
 
 func _physics_process(_delta):
-	if Player == null:
-		Player = get_node_or_null("/root/Game/Player")
-	if Player != null:
+	playerPosition = Player.global_transform.origin #error: invalid get index 'global_position' (on base: 'PackedScene')
+
+	if detectedPlayer == false: #if player undetected, do nothing
+		pass
+
+	if detectedPlayer == true: #if player detected, follow them
 		look_at(Player.global_transform.origin, Vector3.UP)
-	
-	
+		var dir = (playerPosition - enemyPosition).normalized() #direction enemy is going to player
+		move_and_collide(dir * -speed * _delta) #moving speed
 	
 
 
 
 func _on_Area_body_entered(body):
 	if body.name == "Player":
+		detectedPlayer = true
 		var sound = get_node_or_null("/root/Game/Robot")
 		if sound != null:
 			sound.playing = true
